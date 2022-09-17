87f37a22abac5c88005229cf7d159285a302c1cc
Prince Mishra <princemishra.aarya@gmail.com>
2022-09-17 18:40:13 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+  say = "Hello World";
+
+  greet() {
+    return "Good Morning"
+  }
+
+  add(num1: number, num2: number) {
+    return num1 + num2
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment.prod"
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+platformBrowserDynamicTesting().bootstrapModule(AppModule)
+.catch(err => console.error(err));
 
-if (environment.production) {
+if(environment.production){
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
