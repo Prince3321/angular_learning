e5f2e2575242e2e12755c261384889420df9d910
Prince Mishra <princemishra.aarya@gmail.com>
2022-09-17 23:12:37 +0530

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-project';
   say = "Hello World";
+  alt = '';
+  imgURL = 'https://picsum.photos/id/5/500/500'
 
   greet() {
     return "Good Morning"
@@ -15,5 +17,15 @@ export class AppComponent {
 
   add(num1: number, num2: number) {
     return num1 + num2
+  }
+
+  getImage() {
+    this.alt = ''
+    this.imgURL = 'https://picsum.photos/id/237/200/300'
+  }
+
+  changeImage(event: KeyboardEvent) {
+    this.alt = ''
+    this.imgURL = (event.target as HTMLInputElement).value
   }
 }
